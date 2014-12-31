5657532cc0a34f2b330c0e6f2b58949bb09c6852
Shannon Smith <shannones2013@gmail.com>
2014-12-30 23:22:41 -0800

    js

--- js/app.js
+++ js/app.js
@@ -25,6 +25,19 @@ $(document).ready(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
+   $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      
+      $('.ryu-ready').hide();
+      $('.ryu-cool').show();
+    }   
+  })
+   .keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  });
 });
 
 function playHadouken () {
